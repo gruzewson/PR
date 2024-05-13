59844d899e7578b33747652f2d5dab65bcb45eef
gruzewson <114955286+gruzewson@users.noreply.github.com>
2024-05-13 13:25:59 +0200

    Add files via upload

---
+++ lab7/display.c
@@ -0,0 +1,29 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#define INPUT_BUFFER_SIZE 64
+#define EXIT 0
+#define FIFO "mypipe"
+
+int main() {
+  int mypipe;
+  mypipe = open(FIFO, O_RDONLY);
+  while (1) {
+    int status;
+    printf("DISPLAY: Waiting for message.\n");
+    read(mypipe, &status, sizeof(status));
+
+    if (status == EXIT) {
+      puts("DISPLAY: quits.\n");
+      close(mypipe);
+      exit(0);
+    }
+    char message[INPUT_BUFFER_SIZE];
+
+    read(mypipe, &message, sizeof(message));
+    printf("DISPLAY: Reads '%s' from named pipe.\n", message);
+  }
+}
--- lab7/main.c
+++ lab7/main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 #define READ 0
 #define WRITE 1
@@ -10,13 +12,19 @@
 #define WORK 1
 #define EXIT 0
 
-void transformation(char* str) {
+void transformation_letters(char* str) {
     for (int i = 0; i < strlen(str); i++) {
         if (str[i] >= 'a' && str[i] <= 'z') {
             str[i] -= 32;
         } else if (str[i] >= 'A' && str[i] <= 'Z') {
             str[i] += 32;
-        } else if (str[i] >= '0' && str[i] <= '9') {
+        }
+    }
+}
+
+void transformation_numbers(char* str) {
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
             str[i] = '.';
         }
     }
@@ -25,51 +33,84 @@ void transformation(char* str) {
 void input(int pipe1[2], int pipe2[2], int pid, int* status) {
     close(pipe1[READ]);
     close(pipe2[WRITE]);
+    int status = CONTINUE;
     char message[INPUT_BUFFER_SIZE];
     printf("INPUT: enter a message: (q if you want to exit)\n");
     scanf("%s", message);
-
     if (strcmp(message, "q") == 0) {
-        *status = EXIT;
+        status = EXIT;
+    }
+
+    write(pipe1[WRITE], &status, sizeof(status));
+
+    if (status == EXIT) {
         printf("INPUT: quits.\n");
         close(pipe1[WRITE]);
-        close(pipe2[READ]);
-        return;
+        waitpid(pid, NULL, 0);
+        exit(0);
     }
     write(pipe1[WRITE], message, sizeof(message));
 }
 
-void transform(int pipe1[2], int pipe2[2], int pid) {
+void transform1(int pipe1[2], int pipe2[2]) {
+int status;
+    read(pipe1[READ], &status, sizeof(status));
     close(pipe1[WRITE]);
     close(pipe2[READ]);
-
+    if(status == EXIT) {
+        write(pipe2[WRITE], &status, sizeof(status));
+        close(pipe1[READ]);
+        close(pipe2[WRITE]);
+        waitpid(pid, NULL, 0);
+        exit(0);
+    }
     char message[INPUT_BUFFER_SIZE];
     char old_message[INPUT_BUFFER_SIZE];
-    read(pipe1[READ], message, sizeof(message));
     strcpy(old_message, message);
-    transformation(message);
+    transformation_letters(message);
+    
     printf("TRANSFORM: transforms '%s' to '%s'.\n", old_message, message);
 
-    write(pipe2[WRITE], message, sizeof(message));
+    write(pipe2[WRITE], &status, sizeof(status));
+
+    write(pipe2[WRITE], &message, sizeof(message));
 }
 
-void display(int pipe1[2], int pipe2[2]) {
+void transform2(int pipe1[2], int pipe2[2]) {
+    int mypipe;
+    mypipe = open(FIFO, O_WRONLY);
     close(pipe1[READ]);
-    close(pipe1[WRITE]);
     close(pipe2[WRITE]);
+    close(pipe2[WRITE]);
+    int status;
+    read(pipe2[READ], &status, sizeof(status));
+    if(status == EXIT) {
+        write(mypipe, &status, sizeof(status));
+        close(pipe1[READ]);
+        close(pipe2[WRITE]);
+        close(mypipe);
+        exit(0);
+    }
     char message[INPUT_BUFFER_SIZE];
-    read(pipe2[READ], message, sizeof(message));
+    char old_message[INPUT_BUFFER_SIZE];
+    strcpy(old_message, message);
+    transformation_numbers(message);
 
-    printf("DISPLAY: has read '%s'.\n", message);
+    printf("TRANSFORM: transforms '%s' to '%s'.\n", old_message, message);
+
+    write(mypipe, &status, sizeof(status));
+
+    write(mypipe, &message, sizeof(message));
 }
 
 int main() {
+  
+    int status = WORK;
     while (1) {
         int pipe1[2];
         pipe(pipe1);
         int pipe2[2];
         pipe(pipe2);
-        int status = WORK;
 
         int pid = fork();
         if (pid == -1) {
@@ -78,24 +119,18 @@ int main() {
         }
 
         if (pid != 0) {
-            input(pipe1, pipe2, pid, &status);
-            if (status == EXIT) {
-                break;
-            }
-            wait(NULL);
-        } else{
+            input(pipe1, pipe2, pid);
+        } else {
             int pid2 = fork();
             if (pid2 == -1) {
-                perror("fork() failed");
-                exit(1);
+            perror("fork() failed");
+            exit(1);
             }
 
             if (pid2 != 0) {
-                transform(pipe1, pipe2, pid2);
-                exit(0);
+                transform1(pipe1, pipe2, pid2);
             } else {
-                display(pipe1, pipe2);
-                exit(0);
+                transform2(pipe1, pipe2);
             }
         }
     }
